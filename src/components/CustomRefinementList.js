a91b24bd9bd1c29684b73e1b9ed018675e0f9e68
Brandon Frye <bfrye@centerline.net>
2021-02-28 12:41:46 -0500

    fix formatting

--- src/components/CustomRefinementList.js
+++ src/components/CustomRefinementList.js
@@ -6,14 +6,14 @@ import {
 const RefinementList = ({ items, currentRefinement, refine }) => (
   <ul className="ais-RefinementList-list">
     {items.map(item => (
-        <li
-          key={item.label}
-          className={`ais-RefinementList-item ${item.isRefined ? 'ais-RefinementList-item--selected' : ''} `}
-          onClick={event => {
-            event.preventDefault();
-            refine(item.value);
-          }}
-        >
+      <li
+        key={item.label}
+        className={`ais-RefinementList-item ${item.isRefined ? 'ais-RefinementList-item--selected' : ''} `}
+        onClick={event => {
+          event.preventDefault();
+          refine(item.value);
+        }}
+      >
         <span className="ais-RefinementList-label">
           <span className="ais-RefinementList-labelText">{item.label}</span>
           <span className="ais-RefinementList-count">({item.count})</span>
